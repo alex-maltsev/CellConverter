49a491a68a59cc550bd0713c0f7fcd200ae2fb84
Alexander Maltsev <alex@zulaapp.com>
2016-03-18 15:48:34 -0700

    Made XML pretty printing drop junk new-lines

--- ElementTree_pretty.py
+++ ElementTree_pretty.py
@@ -5,5 +5,5 @@ def prettify(elem):
     """Return a pretty-printed XML string for the Element.
     """
     rough_string = ElementTree.tostring(elem, 'utf-8')
-    reparsed = minidom.parseString(rough_string)
-    return reparsed.toprettyxml(indent="  ")
+    reparsed = minidom.parseString(rough_string).toprettyxml(indent="  ")
+    return '\n'.join([line for line in reparsed.split('\n') if line.strip()])
