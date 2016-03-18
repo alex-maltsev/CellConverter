4b02cc5bd6b7094c445a11ecebf2e768b0b93116
Alexander Maltsev <alex@zulaapp.com>
2016-03-18 00:11:49 -0700

    Copying certain tags directly over from table cell to collection cell

--- basic_converter.py
+++ basic_converter.py
@@ -6,11 +6,36 @@ from ElementTree_pretty import prettify
 
 with open('xib/TableViewCell.xib', 'rt') as f:
     inputTree = etree.parse(f)
-
 inputRoot = inputTree.getroot()
+
+# Several things can be simply copied over
+# The root "document" tag
 outputRoot = Element(inputRoot.tag, inputRoot.attrib)
 
-dependencies = inputRoot.find('./dependencies')
+# "dependencies" tag
+dependencies = inputRoot.find('dependencies')
 outputRoot.append(dependencies)
 
+# "objects" tag with only "placeholder" tags under it
+outputObjects = SubElement(outputRoot, 'objects')
+objects = inputRoot.find('objects')
+for node in objects:
+	if node.tag == 'placeholder':
+		outputObjects.append(node)
+
+# Grabbing objects that we'll need to modify / reshuffle to create collection view cell XIB
+tableCell = objects.find('tableViewCell')
+tableCellContentView = tableCell.find('tableViewCellContentView')
+subviews = tableCellContentView.find('subviews')
+constraints = tableCellContentView.find('constraints')
+
+# Creating collection view cell element, to be built up later
+collectionCell = Element('collectionViewCell')
+outputObjects.append(collectionCell)
+
+# Directly copying over some tags from tableCell to collectionCell
+for tag in [ 'rect', 'autoresizingMask', 'color', 'connections', 'point' ]:
+	node = tableCell.find(tag)
+	collectionCell.append(node)
+	
 print prettify(outputRoot)
