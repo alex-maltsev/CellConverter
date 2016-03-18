e906022048bda6edf071b92ef90a51359cd1013c
Alexander Maltsev <alex@zulaapp.com>
2016-03-18 11:10:49 -0700

    Giving attributes to collection view cell tag

--- basic_converter.py
+++ basic_converter.py
@@ -28,9 +28,21 @@ tableCell = objects.find('tableViewCell')
 tableCellContentView = tableCell.find('tableViewCellContentView')
 subviews = tableCellContentView.find('subviews')
 constraints = tableCellContentView.find('constraints')
+cellId = tableCell.get('id')
+cellClass = tableCell.get('customClass')
+cellContentViewId = tableCellContentView.get('id')
 
+#print cellId, cellContentViewId, cellClass
+
+# Preparing attributes dictionary for collection view cell with a few generic settings, and important 'id' and 'customClass' settings
+# Using 'id' matching that of tableViewCellContentView, so that constraints can work without alterations
+# Using the same 'customClass' that we pick up from table view cell
+collectionCellAttribs = {
+	'opaque' : 'NO', 'clipsSubviews' : 'YES', 'multipleTouchEnabled' : 'YES', 'contentMode' : 'center',
+	'id' : cellContentViewId, 'customClass' : cellClass
+}
 # Creating collection view cell element, to be built up later
-collectionCell = Element('collectionViewCell')
+collectionCell = Element('collectionViewCell', collectionCellAttribs)
 outputObjects.append(collectionCell)
 
 # Directly copying over some tags from tableCell to collectionCell
