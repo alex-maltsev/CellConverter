dac9e3265ae67c63fdd7edbbf8ac31b4a83ce60c
Alexander Maltsev <alex@zulaapp.com>
2016-03-19 10:57:44 -0700

    Increased indentation to four spaces to match formatting of xib files created by XCode

--- ElementTree_pretty.py
+++ ElementTree_pretty.py
@@ -5,7 +5,7 @@ def prettify(elem):
     """Return a pretty-printed XML string for the Element.
     """
     rough_string = ElementTree.tostring(elem, 'utf-8')
-    reparsed = minidom.parseString(rough_string).toprettyxml(indent="  ")
+    reparsed = minidom.parseString(rough_string).toprettyxml(indent="    ")
     nonEmptyLines = [line for line in reparsed.split('\n') if line.strip()]
     
     # Discarding the first line, which is '<?xml version="1.0" ?>
